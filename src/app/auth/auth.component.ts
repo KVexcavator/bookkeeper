41d6e913d642f91355883edb712cafac7157706d
excavator <str_token@mail.ru>
2019-10-17 18:26:37 +0300

    auth: add template and update routing

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bkp-auth',
   templateUrl: './auth.component.html'
 })
 
-export class AuthComponent {
+export class AuthComponent implements OnInit{
+
+  constructor(private route: Router){}
+
+  ngOnInit(){
+    this.route.navigate(['/login'])
+  }
 
 }
