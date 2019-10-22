23d9fae1363b0db62cadcf5bcae4437cae4ef44c
excavator <str_token@mail.ru>
2019-10-22 19:58:12 +0300

    add dates to Bill page

--- src/app/system/bill-page/bill-card/bill-card.component.ts
+++ src/app/system/bill-page/bill-card/bill-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Bill } from '../../shared/models/bill.model';
 
 @Component({
   selector: 'bkp-bill-card',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillCardComponent implements OnInit {
 
+  @Input() bill: Bill;
+  @Input() currency: any;
+
+  dollar: number;
+  euro: number;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.currency)
+    // base EUR
+    const { rates } = this.currency;
+    this.euro = this.bill.value / rates['RUB'];
+    this.dollar = this.bill.value * rates['USD'] / rates['RUB'];
+    
   }
 
 }
--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -11,21 +11,42 @@ import { Bill } from '../shared/models/bill.model';
 })
 export class BillPageComponent implements OnInit, OnDestroy {
 
-  private subcribtion: Subscription;
+  sub_1: Subscription;
+  sub_2: Subscription;
+
+  currency: any;
+  bill: Bill;
+
+  isLoaded = false;
 
   constructor(private billService: BillService) { }
 
   ngOnInit() {
-    this.subcribtion = combineLatest(
+    this.sub_1 = combineLatest(
       this.billService.getBill(),
       this.billService.getCurrency()
     ).subscribe((date: [Bill, any])=>{
-      console.log(date);
+      // console.log(date);
+      this.bill = date[0];
+      this.currency = date[1];
+      this.isLoaded = true;
     })
   }
 
-  ngOnDestroy(){
-    this.subcribtion.unsubscribe();
+  onRefresh(){
+    this.isLoaded = false;
+    this.sub_2 = this.billService.getCurrency()
+      .subscribe((currency: any) => {
+        this.currency = currency;
+        this.isLoaded = true;
+      })
   }
+
+
+  ngOnDestroy(){
+    this.sub_1.unsubscribe();
+    this.sub_2.unsubscribe();
+  }
+
 
 }
