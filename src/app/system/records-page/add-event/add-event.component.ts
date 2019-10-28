a4e20890a8104b8e0ba8e9b746116c16e24430d6
excavator <str_token@mail.ru>
2019-10-28 14:02:41 +0300

    add event in the records-page, part 1

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as moment from 'moment';
+
+import { Category } from '../../shared/models/category.model';
+import { BKPEvent } from '../../shared/models/event.model';
 
 @Component({
   selector: 'bkp-add-event',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types=[
+    { type: 'income', label: 'Доход'},
+    { type: 'outcome', label: 'Расход'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    let { amount, description, type, category} = form.value;
+    if( amount < 0 ) amount *= -1;
+    let date = moment().format('DD.MM.YYYY HH:mm:ss')
+
+    const event = new BKPEvent(
+      type, 
+      amount, 
+      +category, 
+      date, 
+      description, 
+    );
+
+    console.log(event);
   }
 
 }
