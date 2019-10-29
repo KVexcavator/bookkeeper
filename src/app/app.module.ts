835d3de1f187e45b1bd01bc1e3b47a33be176bd1
excavator <str_token@mail.ru>
2019-10-29 19:07:36 +0300

    history-page: ngx-pie-chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
@@ -22,7 +23,7 @@ import { SystemModule } from './system/system.module';
     AuthModule,
     AppRoutingModule,
     SystemModule,
-    
+    BrowserAnimationsModule       
   ],
   providers: [
     UsersService,
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, combineLatest } from 'rxjs';
+
+import { CategoriesService } from '../shared/services/categories.service';
+import { EventsService } from '../shared/services/events.service';
+import { Category } from '../shared/models/category.model';
+import { BKPEvent } from '../shared/models/event.model';
+
 
 @Component({
   selector: 'bkp-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.scss']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  sub_1: Subscription;
+  isLoaded = false;
+  categories: Category[] = [];
+  events: BKPEvent[] = [];
+  chartDate = [];
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private eventsService: EventsService
+  ) { }
 
   ngOnInit() {
+    this.sub_1 = combineLatest(
+      this.categoriesService.getCategories(),
+      this.eventsService.getEvents()
+    ).subscribe((date: [ Category[], BKPEvent[] ]) => {
+      this.categories = date[0];
+      this.events = date[1];
+      this.calculateChartDate();
+      // after complete
+      this.isLoaded = true;
+    })
   }
+
+  calculateChartDate(): void {
+    this.chartDate = [];
+    this.categories.forEach((cat) => {
+      const catEvents = this.events
+        .filter((e) => e.category === cat.id && e.type === 'outcome');
+      this.chartDate.push({
+        "name": cat.name,
+        "value": catEvents.reduce((total,e) => {
+          total += e.amount;
+          return total;
+        },0)
+      })
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.sub_1) this.sub_1.unsubscribe();
+  }
+
 
 }
