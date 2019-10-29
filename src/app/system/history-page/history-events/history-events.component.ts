d785ad2e8d0f8a0ce4656e46f384154b4ae8a203
excavator <str_token@mail.ru>
2019-10-29 15:34:17 +0300

    history-page: scaffolding component

---
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bkp-history-events',
+  templateUrl: './history-events.component.html',
+  styleUrls: ['./history-events.component.scss']
+})
+export class HistoryEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
